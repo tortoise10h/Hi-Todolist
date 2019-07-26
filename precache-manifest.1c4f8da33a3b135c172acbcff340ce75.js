02d9beba47b620587615f3082f5cd57275d663dc
Huy Nguyen <35460173+tortoise10h@users.noreply.github.com>
2019-07-26 15:15:16 +0700

    Add files via upload

---
+++ precache-manifest.1c4f8da33a3b135c172acbcff340ce75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae96e35b8ea73ff25e87994e255260f",
+    "url": "https://tortoise10h.github.io/Hi-Todolist/index.html"
+  },
+  {
+    "revision": "0d3b29a06e449b8553c6",
+    "url": "https://tortoise10h.github.io/Hi-Todolist/static/css/main.9bab759c.chunk.css"
+  },
+  {
+    "revision": "fa84d330438ca97f0145",
+    "url": "https://tortoise10h.github.io/Hi-Todolist/static/js/2.92f0d61b.chunk.js"
+  },
+  {
+    "revision": "0d3b29a06e449b8553c6",
+    "url": "https://tortoise10h.github.io/Hi-Todolist/static/js/main.7a518dfb.chunk.js"
+  },
+  {
+    "revision": "ec47cb47a6269738dcf1",
+    "url": "https://tortoise10h.github.io/Hi-Todolist/static/js/runtime~main.b563edea.js"
+  }
+]);
